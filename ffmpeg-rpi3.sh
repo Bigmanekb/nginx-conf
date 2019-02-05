c087a36cfc919d830b0026771ff5a2b2b64e9585
Bigmanekb <46221443+Bigmanekb@users.noreply.github.com>
2019-02-05 18:57:18 +0500

    Create ffmpeg-rpi3.sh

---
+++ ffmpeg-rpi3.sh
@@ -0,0 +1,13 @@
+cd /usr/src
+git clone git://git.videolan.org/x264
+cd x264
+./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+make -j4
+sudo make install
+cd /usr/src
+git clone https://github.com/FFmpeg/FFmpeg.git
+cd FFmpeg
+sudo apt-get install libomxil-bellagio-dev
+sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --enable-omx --enable-omx-rpi --enable-mmal
+make -j4
+sudo make install
