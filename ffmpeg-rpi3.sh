aaf1e48b845274fd961d36609f5882dda2987e9c
Bigmanekb <46221443+Bigmanekb@users.noreply.github.com>
2019-02-06 01:16:37 +0500

    Update ffmpeg-rpi3.sh

--- ffmpeg-rpi3.sh
+++ ffmpeg-rpi3.sh
@@ -14,3 +14,9 @@ sudo apt-get install libomxil-bellagio-dev
 sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --enable-omx --enable-omx-rpi --enable-mmal
 make -j4
 sudo make install
+sudo apt-get install -y nginx
+mv /etc/nginx/nginx.conf /etc/nginx/nginx_default.conf
+mv /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default_old
+curl https://raw.githubusercontent.com/Bigmanekb/nginx-conf/master/default -o /etc/nginx/sites-enabled/default
+curl https://raw.githubusercontent.com/Bigmanekb/nginx-conf/master/nginx.conf -o /etc/nginx/nginx.conf
+sudo /etc/init.d/nginx start
